554a6c81a1334fb0d24c6069cdb41d81b5ab9623
antiloopDP <969@mail.ru>
2021-05-27 09:38:24 +0300

    lesson_2. добавил links_product для страницы index

--- lesson_1/geekshop/geekshop/views.py
+++ lesson_1/geekshop/geekshop/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
-
 def index(request):
     title = 'geekshop'
 
+    links_product = [
+        {'href': 'index', 'url': 'static/img/product-1.jpg'},
+        {'href': 'index', 'url': 'static/img/product-2.jpg'},
+        {'href': 'index', 'url': 'static/img/product-3.jpg'},
+        {'href': 'index', 'url': 'static/img/product-4.jpg'},
+    ]
     context = {
+        'links_product': links_product,
         'title': title,
     }
+
     return render(request, 'index.html', context=context)
 
 def contact(request):
